a48b6efbc5c64d2be71248fb5302e032a30d506a
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-01-07 17:07:01 +0100

    funzt

---
+++ classes/class-svg-social-menu-widget.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Class Svg_Social_Menu_Widget
+ */
+class Svg_Social_Menu_Widget extends WP_Widget {
+
+	public function __construct() {
+		$widget_ops = array(
+			'classname'   => 'widget-svg-social-menu',
+			'description' => __( 'Widget to display social network profile link as SVGs', 'svg-social-menu' )
+		);
+		parent::__construct( 'svg_social_menu_widget', __( 'SVG Social Menu', 'svg-social-menu' ), $widget_ops );
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? false : $instance['title'] );
+		echo $args['before_widget'];
+		if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		if ( has_nav_menu( 'svg-social-menu' ) ) {
+			wp_nav_menu(
+				array(
+					'theme_location' => 'svg-social-menu',
+					'menu_class'     => 'svg-social-menu',
+					'container'      => '',
+					'walker'         => new Svg_Social_Menu_Walker(),
+					'depth'          => 1
+				)
+			);
+		}
+		echo $args['after_widget'];
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance          = $old_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		$title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+		?>
+		<p><label
+				for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'svg-social-menu' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>"
+			       name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>"/>
+		</p>
+	<?php }
+}
--- svg-social-menu.php
+++ svg-social-menu.php
@@ -9,4 +9,57 @@ Author:      Florian Brinkmann
 Author URI:  https://florianbrinkmann.de
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Domain Path: /languages
+Text Domain: svg-social-menu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+*/
+
+/**
+ * Register Theme Location
+ */
+function svg_social_menu_register_theme_location() {
+	if ( ! array_key_exists( 'svg-social-menu', get_registered_nav_menus() ) ) {
+		register_nav_menu( 'svg-social-menu', __( 'Social Profile Links', 'svg-social-menu' ) );
+	}
+}
+
+add_action( 'after_setup_theme', 'svg_social_menu_register_theme_location' );
+
+/**
+ * Adds the scripts and styles
+ */
+function svg_social_menu_scripts_styles() {
+	if ( has_nav_menu( 'svg-social-menu' ) ) {
+		wp_enqueue_script( 'svg-social-menu-svg4everybody', plugins_url( 'js/svg4everybody.js', __FILE__ ), array( 'jquery' ), false, true );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'svg_social_menu_scripts_styles' );
+
+/**
+ * Adds inline styles to the header
+ */
+
+function svg_social_menu_inline_style() {
+	$styles = '<style>.widget-svg-social-menu ul {padding-left: 0;}.widget-svg-social-menu ul li {float: left;list-style-type: none;margin: 0.7em;line-height: 0;font-size: .9em;}.widget-svg-social-menu ul svg {fill: #444; height: 2em; width: 2em;}.widget-svg-social-menu ul a:hover svg,.widget-svg-social-menu ul a:focus svg,.widget-svg-social-menu ul a:active svg {fill: #999;}</style>';
+	echo apply_filters( 'svg_social_menu_inline_style', $styles );
+}
+
+add_action( 'wp_head', 'svg_social_menu_inline_style' );
+
+require_once 'classes/class-svg-social-menu-walker.php';
+require_once 'classes/class-svg-social-menu-widget.php';
+
+function svg_social_menu_register_widget() {
+	register_widget( 'Svg_Social_Menu_Widget' );
+}
+
+add_action( 'widgets_init', 'svg_social_menu_register_widget' );
