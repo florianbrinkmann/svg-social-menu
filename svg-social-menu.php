dd7a82503367829e4fbd9e518a076b2d953057f3
Florian Brinkmann <kontakt@florianbrinkmann.de>
2016-01-07 14:43:00 +0100

    file header

---
+++ svg-social-menu.php
@@ -0,0 +1,12 @@
+<?php
+defined( 'ABSPATH' ) or die( "Nothing to see!" );
+/*
+Plugin Name: SVG Social Menu
+Plugin URI:
+Description: This describes my plugin in a short sentence
+Version:     1.0
+Author:      Florian Brinkmann
+Author URI:  https://florianbrinkmann.de
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
